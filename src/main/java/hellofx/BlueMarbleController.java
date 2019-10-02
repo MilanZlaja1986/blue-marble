adc9f3236bbccc12a6952ee1d19f52141511140f
OKAN <OKAN@DESKTOP-OAF8CML>
2019-10-02 13:04:02 +0200

    Modified

--- src/main/java/hellofx/BlueMarbleController.java
+++ src/main/java/hellofx/BlueMarbleController.java
@@ -76,6 +76,4 @@ public class BlueMarbleController {
 			alert.showAndWait();
 		}
 		
-	}
-
-}
+	}}
--- src/main/java/hellofx/Launcher.java
+++ src/main/java/hellofx/Launcher.java
@@ -2,7 +2,7 @@ package hellofx;
 
 public class Launcher {
 	    
-	    public static void main(String[] args) {
+	    public static void main(String[] args)  {
 //	    	HelloFX.main(args);
 	        SceneBuilderFX.main(args);
 	    }
